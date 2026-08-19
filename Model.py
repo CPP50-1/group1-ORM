86d527955a17bd7e19abaa2f3d285cd1a6da0e27
GSoMan-dev <ayotte.victor@gmail.com>
2026-08-19 17:00:04 +0200

    [feat] ModelMeta model + table exists exception

--- Model.py
+++ Model.py
@@ -2,15 +2,51 @@ import os
 import psycopg2
 from Field import Field
 
+registry = {}
 
-class Model:
+class ModelMeta(type):
+    """Metaclass to collect fields and register models."""
+    def __new__(mcs, name, bases, attrs):
+        # We don't want to register the base 'Model' class itself
+        if name == "Model":
+            return super().__new__(mcs, name, bases, attrs)
+
+        # 1. Collect all Field instances declared on the class
+        _fields = {}
+        for key, value in attrs.items():
+            if isinstance(value, Field):
+                _fields[key] = value
+
+        # Save the collected fields back to the class attributes for easy access later
+        attrs['_fields'] = _fields
+
+        # 2. Derive the table name (default to lowercase class name if not provided)
+        table_name = attrs.get('_table', name.lower())
+        attrs['_table'] = table_name
+
+        # Create the actual class
+        new_class = super().__new__(mcs, name, bases, attrs)
+
+        # 3. Register the model in the global registry
+        registry[table_name] = new_class
+
+        return new_class
+
+class Model(metaclass=ModelMeta):
     """Base class that developers will inherit from."""
+
+    def __init__(self, **kwargs):
+        # Allow instantiation like User(name="Alice", age=30)
+        for key, value in kwargs.items():
+            if key in self._fields:
+                setattr(self, key, value)
 
     @classmethod
     def create_table(cls, conn):
         # 1. Determine table name (lowercase class name + 's' is standard convention)
         table_name = cls.__name__.lower() + "s"
-        # todo check existing? https://stackoverflow.com/questions/20582500/how-to-check-if-a-table-exists-in-a-given-schema
+        if table_name in registry:
+            raise Exception(f"Table '{table_name}' already exists!")
 
         columns = []
         values = []
